df4ae7c015aa7ed506f7c57c012097c0bad00ca9
Bruno Tardio <brunotardiosi@gmail.com>
2024-10-15 22:22:14 -0300

    ajustes

--- GarageApi/GarageApi.WS.GerenciadorDeUsuarios/Program.cs
+++ GarageApi/GarageApi.WS.GerenciadorDeUsuarios/Program.cs
@@ -3,9 +3,14 @@ using GarageApi.WS.GerenciadorDeUsuarios.Repositorio;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Registra as configurações do MongoDB
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings"));
+
+// Registra o MongoDbContext
+builder.Services.AddSingleton<MongoDbContext>();
 
 // Adicionando o MongoDbContext ao projeto
-builder.Services.AddSingleton<MongoDbContext>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- GarageApi/GarageApi.WS.GerenciadorDeUsuarios/Repositorio/MongoDbContext.cs
+++ GarageApi/GarageApi.WS.GerenciadorDeUsuarios/Repositorio/MongoDbContext.cs
@@ -1,20 +1,33 @@
 ﻿using Garage.API.Domain.Models;
+using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
 namespace GarageApi.WS.GerenciadorDeUsuarios.Repositorio
 {
+    public class MongoDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
     public class MongoDbContext
     {
-
         private readonly IMongoDatabase _database;
 
-        public MongoDbContext(IConfiguration configuration)
+        public MongoDbContext(IOptions<MongoDBSettings> settings)
         {
-            var client = new MongoClient(configuration.GetSection("MongoDBSettings:ConnectionString").Value);
-            _database = client.GetDatabase(configuration.GetSection("MongoDBSettings:DatabaseName").Value);
+            var connectionString = settings.Value.ConnectionString;
+            var databaseName = settings.Value.DatabaseName;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception("A string de conexão não foi encontrada.");
+            }
+
+            var mongoClient = new MongoClient(connectionString);
+            _database = mongoClient.GetDatabase(databaseName);
         }
 
         public IMongoCollection<Usuario> Usuarios => _database.GetCollection<Usuario>("Usuarios");
-
     }
 }
